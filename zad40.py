5e9792d970b58638c9ccd8a3b08fc82bc1f6a5bd
damianpietrzakofficial <95929672+damianpietrzakofficial@users.noreply.github.com>
2021-12-12 18:36:49 +0100

    Delete zad40.py

--- zad40.py
+++
@@ -1,9 +0,0 @@
-print("Podaj bok trójkąta:")
-bok = (int(input()))
-poziom = bok * "x"
-
-for i in range(bok):
-    wiersz = (bok - (bok -i)) * "x"
-    spacje = (bok - i) * " "
-    print(spacje+wiersz)
-print(poziom)
