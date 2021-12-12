df4a63cd3888edf7c6aec115e52c7334e910f606
damianpietrzakofficial <95929672+damianpietrzakofficial@users.noreply.github.com>
2021-12-12 18:37:49 +0100

    Delete zad9.py

--- zad9.py
+++
@@ -1,9 +0,0 @@
-bombka = "*"
-print("    ^")
-print("   ^^^")
-print("  ^", bombka ,"^^^", sep="")
-print(" ^^^^^", bombka ,"^", sep="")
-print("  ^^^^^")
-print(" ^^", bombka, "^^^^", sep="")
-print("^^^^^^", bombka, "^^", sep="")
-print("   ###")
